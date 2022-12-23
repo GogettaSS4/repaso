f43f9a8eead469683d757c6c204effc75ba1335f
GogettaSS4 <119690881+GogettaSS4@users.noreply.github.com>
2022-12-23 19:14:35 +0100

    Commit

--- r/src/main/java/r/r/R.java
+++ r/src/main/java/r/r/R.java
@@ -12,7 +12,7 @@ package r.r;
 public class R {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo!");
         System.out.println("Que hay de nuevo viejo");
     }
 }
